1afe5ac8728ca44c5cc433fd1cefe36d6197608b
Simon Heather <32168619+X-Guardian@users.noreply.github.com>
2022-12-15 01:28:18 +0000

    New/Set-GitHubRepository: Add AllowAutoMerge and UseSquashPrTitleAsDefault Parameters (#358)
    Adds the following parameters to the `New-GitHubRepository` and `Set-GitHubRepository` functions:
    
    * AllowAutoMerge
    * UseSquashPrTitleAsDefault
    
    References
    
    * https://docs.github.com/en/rest/repos/repos#create-an-organization-repository
    * https://docs.github.com/en/rest/repos/repos#update-a-repository
    
    Fixes #357

--- Tests/GitHubRepositories.tests.ps1
+++ Tests/GitHubRepositories.tests.ps1
@@ -119,6 +119,8 @@ try
                         DisallowSquashMerge = $true
                         DisallowMergeCommit = $true
                         DisallowRebaseMerge = $false
+                        AllowAutoMerge = $true
+                        UseSquashPrTitleAsDefault = $true
                         DeleteBranchOnMerge = $true
                         GitIgnoreTemplate = $testGitIgnoreTemplate
                         LicenseTemplate = $testLicenseTemplate
@@ -142,6 +144,8 @@ try
                     $repo.allow_squash_merge | Should -BeFalse
                     $repo.allow_merge_commit | Should -BeFalse
                     $repo.allow_rebase_merge | Should -BeTrue
+                    $repo.allow_auto_merge | Should -BeTrue
+                    $repo.use_squash_pr_title_as_default | Should -BeTrue
                     $repo.delete_branch_on_merge | Should -BeTrue
                     $repo.is_template | Should -BeTrue
                 }
@@ -170,6 +174,8 @@ try
                         DisallowSquashMerge = $true
                         DisallowMergeCommit = $false
                         DisallowRebaseMerge = $true
+                        AllowAutoMerge = $false
+                        UseSquashPrTitleAsDefault = $false
                     }
                     $repo = New-GitHubRepository @newGitHubRepositoryParms
                 }
@@ -183,6 +189,8 @@ try
                     $repo.allow_squash_merge | Should -BeFalse
                     $repo.allow_merge_commit | Should -BeTrue
                     $repo.allow_rebase_merge | Should -BeFalse
+                    $repo.allow_auto_merge | Should -BeFalse
+                    $repo.use_squash_pr_title_as_default | Should -BeFalse
                 }
 
                 AfterAll -ScriptBlock {
@@ -729,6 +737,8 @@ try
                         DisallowMergeCommit = $true
                         DisallowRebaseMerge = $false
                         DeleteBranchOnMerge = $true
+                        AllowAutoMerge = $true
+                        UseSquashPrTitleAsDefault = $true
                         IsTemplate = $true
                     }
 
@@ -750,6 +760,8 @@ try
                     $updatedRepo.allow_squash_merge | Should -BeFalse
                     $updatedRepo.allow_merge_commit | Should -BeFalse
                     $updatedRepo.allow_rebase_merge | Should -BeTrue
+                    $updatedRepo.allow_auto_merge | Should -BeTrue
+                    $updatedRepo.use_squash_pr_title_as_default | Should -BeTrue
                     $updatedRepo.delete_branch_on_merge | Should -BeTrue
                     $updatedRepo.is_template | Should -BeTrue
                 }
@@ -763,6 +775,8 @@ try
                         DisallowSquashMerge = $true
                         DisallowMergeCommit = $false
                         DisallowRebaseMerge = $true
+                        AllowAutoMerge = $false
+                        UseSquashPrTitleAsDefault = $false
                     }
 
                     $updatedRepo = Set-GitHubRepository @updateGithubRepositoryParms -PassThru
@@ -777,6 +791,8 @@ try
                     $updatedRepo.allow_squash_merge | Should -BeFalse
                     $updatedRepo.allow_merge_commit | Should -BeTrue
                     $updatedRepo.allow_rebase_merge | Should -BeFalse
+                    $updatedRepo.use_squash_pr_title_as_default | Should -BeFalse
+                    $updatedRepo.delete_branch_on_merge | Should -BeFalse
                 }
             }
 
